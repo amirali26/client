ca2f344f6a9fba23d3cf5d276063a5f1c3d336e5
amirali26 <amiralishojaee123@gmail.com>
2022-02-26 14:25:21 +0000

    wip

--- Account/AccountDto.cs
+++ Account/AccountDto.cs
@@ -10,6 +10,9 @@ namespace client.Account
         public string Website { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
+        public string PostCode { get; set; }
+        public string Region { get; set; }
+        public string AreaInRegion { get; set; }
         public DateTime RegisteredDate { get; set; }
         public IEnumerable<string> AreasOfPractice { get; set; }
     }
